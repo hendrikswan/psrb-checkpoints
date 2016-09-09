6fcb91c400d8a0f423233046b13eb0222435ab10
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-09 16:44:43 +0200

    Remove default example

--- app/i18n.js
+++ app/i18n.js
@@ -7,25 +7,16 @@
 import { addLocaleData } from 'react-intl';
 
 import enLocaleData from 'react-intl/locale-data/en';
-import deLocaleData from 'react-intl/locale-data/de';
-import afLocaleData from 'react-intl/locale-data/af';
-
-addLocaleData(enLocaleData);
-addLocaleData(deLocaleData);
-addLocaleData(afLocaleData);
 
 export const appLocales = [
   'en',
-  'de',
-  'af',
-
 ];
 
 import enTranslationMessages from './translations/en.json';
-import deTranslationMessages from './translations/de.json';
-import afTranslationMessages from './translations/af.json';
 
-export const formatTranslationMessages = (messages) => {
+addLocaleData(enLocaleData);
+
+const formatTranslationMessages = (messages) => {
   const formattedMessages = {};
   for (const message of messages) {
     formattedMessages[message.id] = message.message || message.defaultMessage;
@@ -36,6 +27,4 @@ export const formatTranslationMessages = (messages) => {
 
 export const translationMessages = {
   en: formatTranslationMessages(enTranslationMessages),
-  de: formatTranslationMessages(deTranslationMessages),
-  af: formatTranslationMessages(afTranslationMessages),
 };
