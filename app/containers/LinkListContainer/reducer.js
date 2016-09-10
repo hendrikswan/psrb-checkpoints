d23100d60eeaea39b404fb3c457e410e26ba3e8d
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-10 20:17:31 +0200

    hardcoded some links

--- app/containers/LinkListContainer/reducer.js
+++ app/containers/LinkListContainer/reducer.js
@@ -9,7 +9,15 @@ import {
   DEFAULT_ACTION,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  links: [
+    {
+      description: 'This is the link description',
+      url: 'https://github.com/facebook/react',
+      id: 'fake',
+    },
+  ],
+});
 
 function linkListContainerReducer(state = initialState, action) {
   switch (action.type) {
