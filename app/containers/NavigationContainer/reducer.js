d813c9e10b3fbab58dcdf1c128e05abf21687296
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-10 12:16:40 +0200

    removed default topics from reducer

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -9,20 +9,7 @@ import {
 } from './constants';
 
 const initialState = fromJS({
-  topics: [
-    {
-      name: 'libraries',
-      description: 'links to useful open source libraries',
-    },
-    {
-      name: 'apps',
-      description: 'links to new and exciting apps',
-    },
-    {
-      name: 'news',
-      description: 'links to programming related news articles',
-    },
-  ],
+  topics: [],
 });
 
 function navigationContainerReducer(state = initialState, action) {
