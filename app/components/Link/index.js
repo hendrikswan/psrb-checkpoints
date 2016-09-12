ab17b4eaa61cc289d1b65b1dd9d956616977acc1
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-12 20:24:07 +0200

    Links look pretty decent - still no LinkButton, IconButton, etc.

--- app/components/Link/index.js
+++ app/components/Link/index.js
@@ -12,7 +12,29 @@ import styles from './styles.css';
 function Link({ link }) {
   return (
     <div className={styles.link}>
-      {link.url} - {link.description}
+      <div
+        className={styles.votingContainer}
+      >
+        {link.voteCount}
+      </div>
+
+      <div
+        className={styles.detailsContainer}
+      >
+        <div>
+          <a
+            href={link.url}
+            className={styles.linkAnchor}
+          >
+            {link.url}
+          </a>
+        </div>
+        <div
+          className={styles.description}
+        >
+          {link.description}
+        </div>
+      </div>
     </div>
   );
 }
@@ -22,6 +44,7 @@ Link.propTypes = {
     description: React.PropTypes.string.isRequired,
     url: React.PropTypes.string.isRequired,
     id: React.PropTypes.string.isRequired,
+    voteCount: React.PropTypes.number.isRequired,
   }),
 };
 
