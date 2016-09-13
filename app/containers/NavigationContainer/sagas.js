b691eaf7562e7576c5f97f5c5f69bfb20bff177f
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-13 21:22:46 +0200

    selecting the default topic

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,9 +1,11 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import 'whatwg-fetch';
-import { REQUEST_TOPICS, SELECT_TOPIC } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC, REQUEST_TOPICS_SUCCEEDED } from './constants';
 import { takeLatest } from 'redux-saga';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
 import { push } from 'react-router-redux';
+import selectNavigationContainer from './selectors';
+
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics')
@@ -23,6 +25,20 @@ function* pushTopic(action) {
   yield put(push(`/topics/${action.topic.name}`));
 }
 
+function* selectDefaultTopic() {
+  const state = yield select(selectNavigationContainer());
+  if (!state.selectedTopic) {
+    yield pushTopic({
+      topic: state.topics[0],
+    });
+  }
+}
+
+export function* selectDefaultTopicSaga() {
+  yield* takeLatest(REQUEST_TOPICS_SUCCEEDED, selectDefaultTopic);
+}
+
+
 // Individual exports for testing
 export function* fetchTopicsSaga() {
   yield* takeLatest(REQUEST_TOPICS, fetchTopics);
@@ -36,4 +52,5 @@ export function* selectTopicSaga() {
 export default [
   fetchTopicsSaga,
   selectTopicSaga,
+  selectDefaultTopicSaga,
 ];
