9f59c21fba91c2ab196b18b061f5fa1fb0200270
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-14 13:23:39 +0200

    validation styling sorted

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -7,9 +7,32 @@
 import React from 'react';
 
 import styles from './styles.css';
+import validator from 'email-validator';
+import classNames from 'classnames';
+
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  state = {};
+
+  login = () => {
+    const email = this.emailField.value;
+    if (!validator.validate(email)) {
+      this.setState({
+        errorText: 'Please provide a valid email',
+      });
+    }
+  }
+
   render() {
+    const fieldError = this.state.errorText ? (
+      <div
+        className={styles.errorMessage}
+      >
+        {this.state.errorText}
+      </div>
+    ) : null;
+
+
     return (
       <div className={styles.login}>
         <div
@@ -19,11 +42,13 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
         </div>
 
         <input
-          className={styles.input}
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
           placeholder="Your email"
           ref={(f) => { this.emailField = f; }}
           type="text"
         />
+
+        {fieldError}
 
         <div
           className={styles.actionContainer}
@@ -35,6 +60,7 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
           </div>
           <div
             className={styles.button}
+            onClick={this.login}
           >
             log in
           </div>
