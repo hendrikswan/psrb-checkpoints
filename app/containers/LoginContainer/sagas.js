29b3d26978f2d057e75a3812f15d45e20c51df6a
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-14 14:07:07 +0200

    going back after successful login

--- app/containers/LoginContainer/sagas.js
+++ app/containers/LoginContainer/sagas.js
@@ -1,11 +1,20 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { LOGIN } from './constants';
+import { put } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { goBack } from 'react-router-redux';
+
+function* handleLogin() {
+  yield put(goBack());
+}
+
 
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* doLoginSaga() {
+  yield* takeLatest(LOGIN, handleLogin);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  doLoginSaga,
 ];
