d12ea0611db05c9185d0b46ffdf981a279264646
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-14 12:56:50 +0200

    login form looking okay

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -6,15 +6,42 @@
 
 import React from 'react';
 
-
 import styles from './styles.css';
 
-function Login() {
-  return (
-    <div className={styles.login}>
-      This is the Login component
-    </div>
-  );
+class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className={styles.login}>
+        <div
+          className={styles.heading}
+        >
+          Login with your email
+        </div>
+
+        <input
+          className={styles.input}
+          placeholder="Your email"
+          ref={(f) => { this.emailField = f; }}
+          type="text"
+        />
+
+        <div
+          className={styles.actionContainer}
+        >
+          <div
+            className={styles.button}
+          >
+            cancel
+          </div>
+          <div
+            className={styles.button}
+          >
+            log in
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
